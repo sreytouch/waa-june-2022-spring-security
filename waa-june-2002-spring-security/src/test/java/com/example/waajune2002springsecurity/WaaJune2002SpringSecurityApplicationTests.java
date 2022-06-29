5e782269201fd7c72f8e8bf5fe7cc1604a2f3aa0
sreytouch <atouch.it@gmail.com>
2022-06-29 17:04:04 -0500

    lab6

---
+++ waa-june-2002-spring-security/src/test/java/com/example/waajune2002springsecurity/WaaJune2002SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.waajune2002springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaaJune2002SpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
